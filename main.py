de4804e4b0f91fae402225de349e6c333dc89b1d
nerazull <dragosnboston@gmail.com>
2026-01-30 16:32:48 +0200

    Add conversation messages list for Gemini API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 
 load_dotenv()
@@ -15,8 +16,10 @@ parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt
+    model='gemini-2.5-flash', contents=messages
 )
 
 if response.usage_metadata is None:
