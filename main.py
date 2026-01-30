d3195b128cf8906c98b8e2281d410de6237fcf62
nerazull <dragosnboston@gmail.com>
2026-01-30 15:33:38 +0200

    Print Gemini API prompt and response token counts

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
+if api_key is None:
     raise RuntimeError("Environment variable not found.")
 
 client = genai.Client(api_key=api_key)
@@ -13,4 +13,10 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
+
+if response.usage_metadata is None:
+    raise RuntimeError("No usage metadata to display.")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
