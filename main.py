89ee107db25ddd0d88db434246980ed3e9c62682
nerazull <dragosnboston@gmail.com>
2026-01-30 15:14:48 +0200

    Integrate Gemini API client and load key from environment variable

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from gemini-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("Environment variable not found.")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
