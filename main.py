7e923428b9d4fbb24ba2f46242e4461928fa2126
nerazull <dragosnboston@gmail.com>
2026-01-30 16:41:43 +0200

    Support verbose mode with prompt and token usage stats

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -24,7 +25,11 @@ response = client.models.generate_content(
 
 if response.usage_metadata is None:
     raise RuntimeError("No usage metadata to display.")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+else:
+    print(response.text)
