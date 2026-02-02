f251d8ba081702172aa5c77d07e4568fbdb42957
nerazull <dragosnboston@gmail.com>
2026-02-02 14:01:03 +0200

    Add Gemini function declarations and tool config for file listing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 import argparse
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 
 load_dotenv()
@@ -21,7 +22,7 @@ args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt)
+    model='gemini-2.5-flash', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
 )
 
 if response.usage_metadata is None:
@@ -31,6 +32,10 @@ if args.verbose:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    #print(response.text)
+
+if response.function_calls:
+    for func_call in response.function_calls:
+        print(f"Calling function: {func_call.name}({func_call.args})")
 else:
     print(response.text)
